0175ca5f23a5be8b12c43b44720a37e2bd3d458a
Maya Minatsuki <mayaeh@marimo-net.org>
2016-09-23 21:56:37 +0900

    ・tweet 取得数を外部から与えられるよう変更 ・DB に取り込む値を配列に格納し返す処理を追加 ・ReTweet の場合の処理分岐を暫定で製作。方針を決め次第修正予定

--- include/maya_lib/tweet_get.inc.php
+++ include/maya_lib/tweet_get.inc.php
@@ -1,6 +1,6 @@
 <?php
 
-function tweet_get() {
+function tweet_get($count) {
 
 	$connection = new Abraham\TwitterOAuth\TwitterOAuth 
 		(TWITTER_CONSUMER_KEY, 
@@ -8,35 +8,96 @@ function tweet_get() {
 		TWITTER_OAUTH_ACCESS_TOKEN, 
 		TWITTER_OAUTH_ACCESS_TOKEN_SECRET);
 
+	if(!isset($count)) {
+
+		$count = 3;
+	}
 
 //	$content = $connection -> get 
 //		("statuses/user_timeline", ["count" => 1, "page" => 1]);
 
-	$content = $connection->get
+	$content = $connection -> get
 		("search/tweets", ["q" => "#エオスタグラム", 
-		"count" => "2"]);
+		"count" => $count, "locale" => "ja"]);
 
-		if ($connection -> getLastHttpCode() == 200) {
-		// Tweet posted succesfully
+	if ($connection -> getLastHttpCode() == 200) {
+	// Tweet posted successfully
 
-//			$screen_name = 
-//				$statuses -> user -> screen_name;
+//		$screen_name = 
+//			$statuses -> user -> screen_name;
 
-//			$id = $statuses -> id_str;
+//		$id = $statuses -> id_str;
 
-//			if (isset($screen_name) and isset($id)) {
+//		if (isset($screen_name) and isset($id)) {
 
-//				$tweet_url = 'https://twitter.com/'. 
-//					$screen_name . '/status/'. 
-//					$id;
+//			$tweet_url = 'https://twitter.com/'. 
+//				$screen_name . '/status/'. 
+//				$id;
 
-//				unset($statuses);
-//				unset($connection);
-
-			$content_array = $content -> statuses;
+//			unset($content);
+//			unset($connection);
+//		}
 
 // for debug
-return $content_array [0] -> text;
+//return $content -> statuses [0] -> text;
+//return $content;
+
+
+		$tweet_array = array();
+
+		$status = null;
+
+		$media_url_array = array();
+
+		$media_array = null;
+
+		foreach ($content -> statuses as $status) {
+
+			if (array_get_value($status, "retweeted_status", "")) {
+
+				foreach ($status -> entities -> media as 
+					$media_array) {
+					
+					$media_url_array[] = 
+						$media_array -> media_url_https;
+				}
+
+				$tweet_array[] = array(
+					"status_id" => $status -> id, 
+					"text" => $status -> text, 
+					"media_url_array" => $media_url_array, 
+					"created_at" => $status -> created_at, 
+					"screen_name" => $status -> user -> screen_name, 
+					"user_id" => $status -> user -> id, 
+					"user_name" => $status -> user -> name, 
+					"profile_image" => $status -> user -> profile_image_url_https
+				);
+			}
+			else {
+
+				foreach ($status -> entities -> media as 
+					$media_array) {
+					
+					$media_url_array[] = 
+						$media_array -> media_url_https;
+				}
+
+				$tweet_array[] = array(
+					"status_id" => $status -> id, 
+					"text" => $status -> text, 
+					"media_url_array" => $media_url_array, 
+					"created_at" => $status -> created_at, 
+					"screen_name" => $status -> user -> screen_name, 
+					"user_id" => $status -> user -> id, 
+					"user_name" => $status -> user -> name, 
+					"profile_image" => $status -> user -> profile_image_url_https
+				);
+			}
+
+			unset($content);
+			unset($connection);
+
+			return $tweet_array;
 
 
 		}
