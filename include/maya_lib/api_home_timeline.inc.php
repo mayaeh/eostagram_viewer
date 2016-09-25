74791e5ae37138b8515268eb00a656afa9519ca3
Maya Minatsuki <mayaeh@marimo-net.org>
2016-09-26 00:32:25 +0900

    home_timeline API の処理内容を製作中

--- include/maya_lib/api_home_timeline.inc.php
+++ include/maya_lib/api_home_timeline.inc.php
@@ -1,15 +1,65 @@
 <?php
 
-function api_home_timeline($count = null, $since_id = null, $max_id = null, $trim_user = null, $exclude_retweet = null) {
+function api_home_timeline($count = null, $since_id = null, $max_id = null, $screen_name = null, $exclude_retweet = null) {
 
 // for debug
 //return array(
 //	'count' => $count, 
 //	'since_id' => $since_id, 
 //	'max_id' => $max_id, 
-//	'trim_user' => $trim_user, 
+//	'screen_name' => $screen_name, 
 //	'exclude_retweet' => $exclude_retweet
 //);
+
+	if (!isset($count)) {
+
+		$count = 5;
+	}
+
+	$query = "SELECT * FROM eostagram_tweet ";
+
+	$query_where_array = array();
+
+	if (isset($since_id)) {
+
+		$query_where_array[] = 
+			"status_id > " . $since_id . " ";
+	}
+
+	if (isset($max_id)) {
+
+		$query_where_array[] = 
+			"status_id < " . $max_id . " ";
+	}
+
+	if (isset($screen_name)) {
+
+		$query_where_array[] = 
+			"screen_name = " . $screen_name . " ";
+	}
+
+	if (isset($exclude_retweet)) {
+
+		$query_where_array[] = 
+			"rt_status_id IS NULL ";
+	}
+
+	if(array_get_value($query_where_array, "0", "")) {
+
+		$query .= "WHERE " . 
+			implode("AND ", $query_where_array);
+	}
+
+	$query .= "ORDER BY status_id DESC LIMIT " . 
+		$count . ";";
+
+// for debug
+return $query;
+
+
+// date('Y-m-d H:i:s',strtotime($created_at))
+// で綺麗に取れる
+
 
 }
 
