af071f4d3b26a2e069f325d66d8461ad2f4338dd
Maya Minatsuki <mayaeh@marimo-net.org>
2016-09-27 07:41:26 +0900

    ・設定用のクッキーが未設定の場合設定する処理を追加 ・HTML ソース上の configContainer の位置を移動

--- index.php
+++ index.php
@@ -1,13 +1,22 @@
 <?php
 require_once ('config.php');
 
+if(isset($_COOKIE)) {
+
+	if(!array_get_value($_COOKIE, "exclude_retweet", "")) {
+
+		setcookie("exclude_retweet","yes",time()+60*60*24*30);
+
+	}
+}
+
 require_once (HTML_HEADER_FILE);
 
 require_once (HTML_TOP_MENU_FILE);
 
-require_once (HTML_CONFIG_PAGE_FILE);
-
 require_once (HTML_BASE_FILE);
+
+require_once (HTML_CONFIG_PAGE_FILE);
 
 require_once (HTML_FOOTER_FILE);
 
