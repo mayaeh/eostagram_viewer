be950107c97b67a06ff57f219104a7c4b6e2a8bf
Maya Minatsuki <mayaeh@marimo-net.org>
2016-09-27 10:31:35 +0900

    Viewer ページ読み込み時に前回の設定を引き継ぐよう変更

--- include/html/html_config_page.inc.php
+++ include/html/html_config_page.inc.php
@@ -1,3 +1,19 @@
+<?php
+
+$checked = 'checked';
+
+if(isset($_COOKIE)) {
+
+	if(array_get_value($_COOKIE, 'exclude_retweet', "")) {
+
+		if($_COOKIE['exclude_retweet'] == 'no') {
+
+			$checked = null;
+		}
+	}
+}
+
+?>
 	<div id="configContainer">
 
 		<div id="config">
@@ -6,7 +22,7 @@
 
 			<span>リツイートを表示しない</span>
 			<div class="switch-wrapper">
-				<input type="checkbox" id="config_exclude_retweet" name="config_exclude_retweet" checked />
+				<input type="checkbox" id="config_exclude_retweet" name="config_exclude_retweet" <?php echo $checked; ?> />
 			</div>
 
 		</div>
