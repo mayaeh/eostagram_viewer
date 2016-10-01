600451ceb82e88b07025b6def58caea83f271f81
Maya Minatsuki <mayaeh@marimo-net.org>
2016-10-01 10:52:25 +0900

    api_home_timeline_html を api 分岐部に追加

--- api/index.php
+++ api/index.php
@@ -6,46 +6,46 @@ if (isset($_GET)) {
 
 //	var_dump($_GET);
 
-$api = 
-$count = 
-$since_id = 
-$max_id = 
-$screen_name =
-$exclude_retweet = null;
+	$api = 
+	$count = 
+	$since_id = 
+	$max_id = 
+	$screen_name =
+	$exclude_retweet = null;
 
 	if(array_get_value($_GET, 'u', "")) {
 
 		$api = htmlspecialchars($_GET['u'], ENT_QUOTES);
 
+		if(array_get_value($_GET, 'count', "")) {
+
+			$count = htmlspecialchars
+				($_GET['count'], ENT_QUOTES);
+		}
+		if(array_get_value($_GET, 'since_id', "")) {
+
+			$since_id = htmlspecialchars 
+				($_GET['since_id'], ENT_QUOTES);
+		}
+		if(array_get_value($_GET, 'max_id', "")) {
+
+			$max_id = htmlspecialchars 
+				($_GET['max_id'], ENT_QUOTES);
+		}
+		if(array_get_value($_GET, 'screen_name', "")) {
+
+			$trim_user = htmlspecialchars 
+				($_GET['screen_name'], ENT_QUOTES);
+		}
+		if(array_get_value($_GET, 'exclude_retweet', "")) {
+
+			$exclude_retweet = htmlspecialchars 
+				($_GET['exclude_retweet'], ENT_QUOTES);
+		}
+
 		switch ($api) {
 
 			case 'home_timeline':
-
-				if(array_get_value($_GET, 'count', "")) {
-
-					$count = htmlspecialchars
-						($_GET['count'], ENT_QUOTES);
-				}
-				if(array_get_value($_GET, 'since_id', "")) {
-
-					$since_id = htmlspecialchars 
-						($_GET['since_id'], ENT_QUOTES);
-				}
-				if(array_get_value($_GET, 'max_id', "")) {
-
-					$max_id = htmlspecialchars 
-						($_GET['max_id'], ENT_QUOTES);
-				}
-				if(array_get_value($_GET, 'screen_name', "")) {
-
-					$trim_user = htmlspecialchars 
-						($_GET['screen_name'], ENT_QUOTES);
-				}
-				if(array_get_value($_GET, 'exclude_retweet', "")) {
-
-					$exclude_retweet = htmlspecialchars 
-						($_GET['exclude_retweet'], ENT_QUOTES);
-				}
 
 				$res = api_home_timeline (
 					$count, $since_id, 
@@ -56,6 +56,20 @@ $exclude_retweet = null;
 //				var_dump ($res);
 
 				header("Content-Type: application/json; charset=utf-8");
+				echo $res;
+
+				break;
+
+			case 'home_timeline_html':
+
+				$res = api_home_timeline_html (
+					$count, $since_id, 
+					$max_id, $screen_name, 
+					$exclude_retweet
+					);
+
+//				var_dump ($res);
+
 				echo $res;
 
 				break;
