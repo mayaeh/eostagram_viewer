93d3c8547e4608d8698eee49b1992714a2c3612e
Maya Minatsuki <mayaeh@marimo-net.org>
2016-10-02 12:38:17 +0900

    import.css を base.css に名称変更、各 CSS ファイルを読み込む記述を追加

--- include/html/html_header.inc.php
+++ include/html/html_header.inc.php
@@ -4,7 +4,16 @@
 <head>
 	<meta charset="UTF-8" />
 	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
-	<link rel="stylesheet" href="./resources/import.css" type="text/css" />
+
+	<link rel="stylesheet" media="all" href="./resources/base.css" type="text/css" />
+
+	<link rel="stylesheet" media="screen and (max-device-width: 480px)" href="./resources/iphone.css" type="text/css" />
+
+	<link rel="stylesheet" media="screen and (min-device-width: 481px) and (max-device-width: 1024px) and (orientation:portrait)" href="./resources/ipad-portrait.css" type="text/css" />
+
+	<link rel="stylesheet" media="screen and (min-device-width: 481px) and (max-device-width: 1024px) and (orientation:landscape)" href="./resources/ipad-landscape.css" type="text/css" />
+
+	<link rel="stylesheet" media="screen and (min-device-width: 1025px)" href="./resources/pc.css" type="text/css" />
 <!--	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script> -->
 	<script type="text/javascript" src="./resources/jquery-3.1.1.min.js"></script>
 	<script type="text/javascript" src="./resources/jquery-ui-1.12.1/jquery-ui.min.js"></script>
@@ -15,7 +24,7 @@
 	<script type="text/javascript" src="./resources/jquery.jscroll.min.js"></script>
 	<script type="text/javascript" src="./resources/main.js"></script>
 	<link rel="icon" type="image/x-icon" href="./favicon.ico" />
-	<title></title>
+	<title>Eostagram-Viewer</title>
 </head>
 <body>
 
